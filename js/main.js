70ba3521965909e6d9f0e6e5108bc5f5a9a1a46b
000.goz <116011109+Samandar0070@users.noreply.github.com>
2023-07-21 06:00:40 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+var userName = prompt("hi enter your name");
+var puli = prompt(userName + " would you like to visit the site? ");
+
+var number=a=10;
+var number=b=5;
+var result = a+b;
+console.log(a+b);
+
+var x = 10;
+var y = 5;
+result = x - y;
+console.log(result);
+
+var c = 25;
+var d = 15;
+console.log(c - d);
+
+var a = 10;
+var b = 5;
+var console = a + b;
+var d = 7;
+console.log(b*h);
+
+var a = 10;
+var b = 3;
+console.log(a % b);
+
+var son = 2;
+var daraja = 10;
+console.log(son ** daraja);
+
